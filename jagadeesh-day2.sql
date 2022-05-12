13bd4524dda840a2cd0e4363b07c2799d84a63bb
jagadeesh67967 <105355483+jagadeesh67967@users.noreply.github.com>
2022-05-12 18:43:15 +0530

    Add files via upload

---
+++ jagadeesh-day2.sql
@@ -0,0 +1,14 @@
+create database day;
+
+use day;
+
+create table dayinfo(id int , name varchar (20) , salary varchar (20));
+
+insert into dayinfo values(1, "jagadeesh",2000);
+
+update dayinfo SET name='vaka':
+
+select * from day;
+
+
+
