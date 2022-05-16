0317d5479902a427b40a518ad89e13a35504678a
santosh67212 <105358745+santosh67212@users.noreply.github.com>
2022-05-16 17:38:44 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,16 @@
+package Training;
+
+public class Test1 {
+	public static void main(String[] args) {
+		int a=80;
+		int b=20;
+		int c=a+b;
+		int d=a-b;
+		int e=a%b;
+		int f=a*b;
+		System.out.println('a' + 'b' + 'c' + 'd' + 'e' + 'f');
+	}
+	
+		
+}
+
