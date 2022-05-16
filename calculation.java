3aa747d921fe03b25c7ba4e0e4f704ff7eacf9b7
vinaysudalagunta <105418856+vinaysudalagunta@users.noreply.github.com>
2022-05-16 17:36:00 +0530

    Add files via upload

---
+++ calculation.java
@@ -0,0 +1,10 @@
+public class calculation {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        System.out.println("addition of two numbers is :" + (a+b));
+        System.out.println("subraction of two numbers is :" + (a-b));
+        System.out.println("multiplication of two numbers is :" + (a*b));
+        System.out.println("division of two numbers is :" + (a/b));
+    }
+}
