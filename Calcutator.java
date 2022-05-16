c71670aac2c7acce0306d79cd529a81f9896d6de
MayankPandita <37814531+MayankPandita@users.noreply.github.com>
2022-05-16 22:26:32 +0530

    Mayank Pandita

---
+++ Calcutator.java
@@ -0,0 +1,15 @@
+package calculator;
+
+public class Calcutator {
+
+	public static void main(String[] args) {
+		int x=15;
+        int y=5;
+        System.out.println("addition of the numbers is :"+(x+y));
+        System.out.println("subtraction of the numbers is :"+(x-y));
+        System.out.println("multiplication of the numbers is :"+(x*y));
+        System.out.println("division of the numbers is :"+(x/y));
+
+	}
+
+}
