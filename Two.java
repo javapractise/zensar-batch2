07d0e8efc5ff6d70d0578f39b3d8d2ea5d81bb5e
MayankPandita <37814531+MayankPandita@users.noreply.github.com>
2022-05-17 18:32:33 +0530

    Add files via upload

---
+++ Two.java
@@ -0,0 +1,22 @@
+package single1;
+
+ 
+	public class Two extends Single  
+	{
+	    public void print_for() { System.out.println("out"); }
+	
+	    public static void main(String[] args)
+	    {
+	        Two g = new Two();
+	        g.print_geek();
+	        g.print_for();
+	       
+	    }
+
+
+
+
+}
+
+
+
