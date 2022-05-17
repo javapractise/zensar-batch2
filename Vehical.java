12f0a481641221fa8be415569acd769706cc4d74
zagabathinisarathkumar <105474233+zagabathinisarathkumar@users.noreply.github.com>
2022-05-17 19:42:43 +0530

    SarathKumar Zagabathuni
    single inheritance

---
+++ Vehical.java
@@ -0,0 +1,9 @@
+package Topic;
+
+public class Vehical {
+	void horn() {
+		System.out.println("press horn");
+	}
+	
+
+}
---
+++ car.java
@@ -0,0 +1,24 @@
+package Topic;
+
+
+
+public class car extends Vehical {
+	
+		void horn() {
+			System.out.println("car horn");
+		}
+	
+
+	public static void main(String[] args) {
+      car c= new car();
+      c.horn();
+      c.horn();
+      
+      
+	
+
+	}
+
+}
+
+
