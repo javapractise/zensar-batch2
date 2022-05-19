d5155d839e010833fd26b6b19bf4fb207baee40b
saichand67211 <105431268+saichand67211@users.noreply.github.com>
2022-05-19 17:38:21 +0530

    Add files via upload

---
+++ Saichand_Day7/lenof_string.java
@@ -0,0 +1,14 @@
+package sai;
+
+public class lenof_string {
+	    public static void main (String[] args){
+	        String  name= new String("saichand");
+
+	        String state = new String("Andhra_Pradesh");
+
+	        System.out.println(name.length());
+
+	         System.out.println(state.length());
+	    }
+
+}
