5dafc9a8bea4224f0d2163ee3fa5c03343a32c88
AK9773 <105356204+AK9773@users.noreply.github.com>
2022-05-19 17:19:39 +0530

    Add files via upload

---
+++ Aman Kumar/StringLength.java
@@ -0,0 +1,18 @@
+
+public class StringLength {
+
+	public static void main(String[] args) {
+		String str ="The escape character is used to escape some of the characters present inside a string.";
+		int a = str.length();
+		System.out.println(" String Length is: "+ a);
+	}
+
+}
+
+
+/*
+
+Output:
+String Length is: 86
+
+*/
