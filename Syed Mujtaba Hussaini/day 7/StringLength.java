4131a20c8311effe4677cc5be5c053ce1dd1c98d
Syed Mujtaba Hussaini <syed.m@zensar.com>
2022-05-19 17:25:54 +0530

    day 7 update

---
+++ Syed Mujtaba Hussaini/day 7/StringLength.java
@@ -0,0 +1,13 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    String s = "Syed";
+	    int i = 0;
+	    for(char c: s.toCharArray()){
+	        i++;
+	    }
+		System.out.println("The length of string without length function is: " + i);
+		System.out.println("The length of string with length function is: " + s.length());
+	}
+}
