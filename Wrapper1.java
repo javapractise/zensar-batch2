4ef4a8b414d165fb3de5b38425bfda6146f3b765
zenPavan <105421076+zenPavan@users.noreply.github.com>
2022-05-19 17:19:50 +0530

    Done All Day 7 Assignment

---
+++ Wrapper1.java
@@ -0,0 +1,14 @@
+//Simple Wrapper Example by Pavan Butke
+
+
+class Test {
+
+    public static void main(String[] args) {
+        // defining int primitive type
+        int num = 10;
+
+        // converting int primitive type into wrapper object
+        Integer numObj = Integer.valueOf(num);
+        System.out.println(numObj);
+    }
+}
