de7af66e3c58e67bfe8223caeb03a6262eb5ad6d
puna1234 <105356038+puna1234@users.noreply.github.com>
2022-05-21 11:49:11 +0530

    Create EnumEx.java

---
+++ Punam Indore day 8/EnumEx.java
@@ -0,0 +1,9 @@
+public class EnumEx {
+	public enum season {WINTER,SPRING,SUMMER,FALL}  // defining enum
+	public static void main(String args[]) {
+		//traversing
+		for(season s:season.values()) 
+			System.out.println(s);
+	}
+
+}
