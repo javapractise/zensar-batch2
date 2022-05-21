d3963e8669c442ec5045e64f0ae98b816922f5c7
puna1234 <105356038+puna1234@users.noreply.github.com>
2022-05-21 11:28:55 +0530

    Create StaticInner.java

---
+++ Punam Indore day 8/StaticInner.java
@@ -0,0 +1,12 @@
+public class StaticInner{
+  static int data=20;
+
+  static class Inner{
+   void msg(){System.out.println("data is "+data);}
+  }
+ 
+  public static void main(String args[]){
+  StaticInner.Inner obj=new StaticInner.Inner();
+  obj.msg();
+  }
+}
