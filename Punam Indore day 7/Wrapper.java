4900d0f3b04a0d9236021d9f16125b9f8522ea8f
puna1234 <105356038+puna1234@users.noreply.github.com>
2022-05-21 10:49:17 +0530

    Create Wrapper.java

---
+++ Punam Indore day 7/Wrapper.java
@@ -0,0 +1,10 @@
+public class Wrappper { 
+  public static void main(String[] args) { 
+    Integer myInt = 6; 
+    Double myDouble = 6.77; 
+    Character myChar = 'p'; 
+    System.out.println(myInt.intValue());
+    System.out.println(myDouble.doubleValue());
+    System.out.println(myChar.charValue());
+  }
+}
