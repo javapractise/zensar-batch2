b189e3035451dfcdda8e0e52036556f213eb1e10
puna1234 <105356038+puna1234@users.noreply.github.com>
2022-05-21 10:53:29 +0530

    Create Unboxing.java

---
+++ Punam Indore day 7/Unboxing.java
@@ -0,0 +1,8 @@
+public class Unboxing{    
+public static void main(String args[]){       
+Integer a=new Integer(6);    
+int i=a.intValue();
+int j=a;
+System.out.println(a+" "+i+" "+j);    
+}
+}  
