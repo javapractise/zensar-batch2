312caf30195ce4e65c107ab160fbbbe033f05209
puna1234 <105356038+puna1234@users.noreply.github.com>
2022-05-22 09:06:39 +0530

    Update Animal.java

--- Punam Indore day 8/Animal.java
+++ Punam Indore day 8/Animal.java
@@ -1,15 +1,19 @@
-class Animal{
-    void
-eatsomething(){system.out.println("eating something");}
-}
-class Dog extends animal{
-    @Override
-    void
-    eatssomething(){system.out.println("eating foods");}//should be something
+class Animal {
+  public void displayInfo() {
+    System.out.println("I am an animal.");
+  }
 }
 
-class TestAnnotation1{
-public static void main(String args[]){
-    Animal a = new Dog();
-    a.eatSomething();
-}}   
+class Lion extends Animal {
+  @Override
+  public void displayInfo() {
+    System.out.println("I am a tiger.");
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    Lion d1 = new Lion();
+    d1.displayInfo();
+  }
+}
