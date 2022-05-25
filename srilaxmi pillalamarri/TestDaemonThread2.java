9e99f974c6793ebcc573e09f893f8520631c9a20
srilaxmipillalamarri <78467693+srilaxmipillalamarri@users.noreply.github.com>
2022-05-25 15:14:33 +0530

    day11_assignment

---
+++ srilaxmi pillalamarri/TestDaemonThread2.java
@@ -0,0 +1,27 @@
+class TestDaemonThread2 extends Thread{  
+ public void run(){  
+  System.out.println("Name: "+Thread.currentThread().getName());  
+  System.out.println("Daemon: "+Thread.currentThread().isDaemon());  
+ }  
+  
+ public static void main(String[] args){  
+  TestDaemonThread2 t1=new TestDaemonThread2();  
+  TestDaemonThread2 t2=new TestDaemonThread2();  
+  t1.start();  
+  t1.setDaemon(true);//will throw exception here  
+  t2.start();  
+ }  
+}  
+
+
+/*
+
+output:
+
+ 
+Name: Thread-0
+Daemon: false
+Exception in thread "main" java.lang.IllegalThreadStateException
+
+
+*/
