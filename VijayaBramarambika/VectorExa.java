2135709678260fdb5f81c0e8666d3a4b3e2e0a2d
Ambika-k <ambikakumpatlas44@gmail.com>
2022-05-25 15:36:55 +0530

    Done with Assignment for day11

---
+++ VijayaBramarambika/VectorExa.java
@@ -0,0 +1,24 @@
+
+import java.util.Iterator;
+import java.util.Vector;
+public class VectorExa{
+	public static void main(String args[]){
+		Vector<String>v= new Vector<String>();
+		v.add("Ayush");
+		v.add("Ramya");
+		v.add("Raju");
+		v.add("Ganesh");
+		Iterator<String>itr=v.iterator();
+		while(itr.hasNext()){
+			System.out.println(itr.next());
+		}
+	}
+}
+
+/*
+Output:
+Ayush
+Ramya
+Raju
+Ganesh
+*/
