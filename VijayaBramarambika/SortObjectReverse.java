945698e49be0b4a961f6de2855d56ce7cae8bdff
Ambika-k <ambikakumpatlas44@gmail.com>
2022-05-26 15:53:22 +0530

    Assignment done for day12

---
+++ VijayaBramarambika/SortObjectReverse.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+public class SortObjectReverse {
+	public static void main(String args[]){
+		ArrayList<String> al=new ArrayList<String>();
+		al.add("viru");
+		al.add ("Saurav");
+		al.add("Mukesh");
+		al.add("Tahir");
+		Collections.sort(al,Collections.reverseOrder());
+		Iterator i=al.iterator();
+		while(i.hasNext())
+		{
+			System.out.println(i.next());
+		}
+	}
+}
+
+
+/*
+Output:
+viru
+Tahir
+Saurav
+Mukesh
+*/
