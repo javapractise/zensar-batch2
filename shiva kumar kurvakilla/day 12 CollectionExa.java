13e2fe3006efc8183abb8f58f3e0fe2dc2c0f674
Shivasiddu <105356809+Shivasiddu@users.noreply.github.com>
2022-05-26 14:49:58 +0530

    assignment

---
+++ shiva kumar kurvakilla/day 12 CollectionExa.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class CollectionExa{
+public static void main(String args[]){
+List<String> list=new ArrayList<String>();
+list.add("shiva");
+list.add("kumar");
+list.add("kurvakilla");
+System.out.println("Initial Collection Value:"+list);
+Crintln("After adding array collection value:"+list);
+}
+}
+
+out put:
+Initial Collection Value:[shiva, kumar, kurvakilla]
+After adding elements collection value:[shiva, kumar, kurvakilla, servelt, Jsp]
+After adding array collection value:[shiva, kumar, kurvakilla, servelt, Jsp, C#, .Net]
