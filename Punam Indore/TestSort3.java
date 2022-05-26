3fcc99909d63eb77c5de1e33b764056b05fcc4ee
puna1234 <105356038+puna1234@users.noreply.github.com>
2022-05-26 16:12:44 +0530

    Create TestSort3.java

---
+++ Punam Indore/TestSort3.java
@@ -0,0 +1,21 @@
+import java.util.*;  
+class TestSort3{  
+public static void main(String args[]){  
+  
+ArrayList al=new ArrayList();  
+al.add(Integer.valueOf(201));  
+al.add(Integer.valueOf(101));  
+al.add(230);//internally will be converted into objects as Integer.valueOf(230)  
+  
+Collections.sort(al);  
+  
+Iterator itr=al.iterator();  
+while(itr.hasNext()){  
+System.out.println(itr.next());  
+ }  
+}  
+}  
+//Output:
+101
+201
+230
