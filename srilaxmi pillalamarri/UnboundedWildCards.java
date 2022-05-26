29cf23f74d569a70dc9524ecc1eed2fd62c0454a
srilaxmipillalamarri <78467693+srilaxmipillalamarri@users.noreply.github.com>
2022-05-26 16:46:42 +0530

    day12_assignment

---
+++ srilaxmi pillalamarri/UnboundedWildCards.java
@@ -0,0 +1,38 @@
+package srilaxmi_pillalamarri;
+
+import java.util.Arrays;
+import java.util.List;
+public class UnboundedWildCards{
+public static void display(List<?>list)
+{
+
+for (Object o:list)
+{
+System.out.println(o);
+}
+}
+
+public static void main(String[] args){
+
+List<Integer>l1=Arrays.asList(1,2,3);
+System.out.println("displaying the integer values");
+display(l1);
+List<String>l2=Arrays.asList("One","Two","Three");
+System.out.println("displaying the string  values");
+display(l2);
+}
+}
+
+
+/* 
+
+output:
+
+3
+displaying the string  values
+One
+Two
+Three
+
+
+*/
