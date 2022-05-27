392790ece586ef87ccbd54a78a167c5cd91798cf
anilghumare <105354744+anilghumare@users.noreply.github.com>
2022-05-27 22:08:29 +0530

    Assignment day 13

---
+++ Anil Ghumare/MysqlCon.java
@@ -0,0 +1,19 @@
+package Example;
+import java.sql.*;
+
+public class MysqlCon {
+	public static void main(String[] args) {
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/customer3","root","root");
+		    Statement stat=con.createStatement();
+			System.out.println("inserting records");
+			String sql="Insert into employee5 values(43)";
+			stat.executeUpdate(sql);
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+	} 
+
+}
