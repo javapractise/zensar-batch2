9984931d9a197b224c6ef448a87471ab70ce6591
zenPavan <105421076+zenPavan@users.noreply.github.com>
2022-05-30 17:18:28 +0530

    100% done

---
+++ PavanButkeMay30/ClassInstanceMethodReference3.java
@@ -0,0 +1,20 @@
+package May30_2;
+
+import java.util.function.BiFunction;  
+class Arithmetic{  
+public int add(int a, int b){  
+return a+b;  
+}  
+}  
+public class InstanceMethodReference3 {  
+public static void main(String[] args) {  
+BiFunction<Integer, Integer, Integer>adder = new Arithmetic()::add;  
+int result = adder.apply(10, 20);  
+System.out.println(result);  
+}  
+}  
+
+
+/*o/p:
+30
+*/
---
+++ PavanButkeMay30/JavaStreamExample.java
@@ -0,0 +1,33 @@
+package May30_2;
+
+import java.util.*;  
+import java.util.stream.Collectors;  
+class Product{  
+    int id;  
+    String name;  
+    float price;  
+    public Product(int id, String name, float price) {  
+        this.id = id;  
+        this.name = name;  
+        this.price = price;  
+    }  
+}  
+public class JavaStreamExample {  
+    public static void main(String[] args) {  
+        List<Product> productsList = new ArrayList<Product>();  
+        //Adding Products  
+        productsList.add(new Product(1,"HP Laptop",25000f));  
+        productsList.add(new Product(2,"Dell Laptop",30000f));  
+        productsList.add(new Product(3,"Lenevo Laptop",28000f));  
+        productsList.add(new Product(4,"Sony Laptop",28000f));  
+        productsList.add(new Product(5,"Apple Laptop",90000f));  
+        List<Float> productPriceList2 =productsList.stream()  
+                                     .filter(p -> p.price > 30000)
+                                     .map(p->p.price)         
+                                     .collect(Collectors.toList());  
+        System.out.println(productPriceList2);  
+    }  
+}  
+
+
+/*o/p:[90000.0]*/
