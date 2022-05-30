624eb2ebe8751d8fa28ce80b47e3f8a76d7ddbd2
srilaxmipillalamarri <78467693+srilaxmipillalamarri@users.noreply.github.com>
2022-05-30 16:18:24 +0530

    day14_assignment

---
+++ srilaxmi pillalamarri/FunctionEx2.java
@@ -0,0 +1,30 @@
+import java.util.function.Function;  
+import java.util.List;  
+import java.util.ArrayList;  
+public class FunctionEx2 {  
+    static Integer addList(List<Integer> list){  
+        return list.stream()  
+                   .mapToInt(Integer::intValue)  
+                   .sum();  
+    }  
+    public static void main(String[] args) {  
+        // Creating a list and adding values  
+        List<Integer> list = new ArrayList<Integer>();  
+        list.add(10);  
+        list.add(20);  
+        list.add(30);  
+        list.add(40);  
+        // Referring addList() method  
+        Function<List<Integer>, Integer> fun = FunctionEx2::addList;  
+        // Calling Function interface method  
+        int result = fun.apply(list);  
+        System.out.println("Sum of list values: "+result);  
+    }  
+}  
+
+/*
+Output:
+Sum of list values: 100
+
+
+*/
