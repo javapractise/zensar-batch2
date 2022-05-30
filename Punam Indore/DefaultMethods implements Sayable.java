062ab99272e6ced14d63aec13f8f918595549e3d
puna1234 <105356038+puna1234@users.noreply.github.com>
2022-05-30 16:27:33 +0530

    Create DefaultMethods implements Sayable.java

---
+++ Punam Indore/DefaultMethods implements Sayable.java
@@ -0,0 +1,22 @@
+interface Sayable{  
+    // Default method   
+    default void say(){  
+        System.out.println("Hello, this is default method");  
+    }  
+    // Abstract method  
+    void sayMore(String msg);  
+}  
+public class DefaultMethods implements Sayable{  
+    public void sayMore(String msg){        // implementing abstract method   
+        System.out.println(msg);  
+    }  
+    public static void main(String[] args) {  
+        DefaultMethods dm = new DefaultMethods();  
+        dm.say();   // calling default method  
+        dm.sayMore("Work is worship");  // calling abstract method  
+  
+    }  
+}  
+
+//Output: Hello, this is default method
+          Work is worship
