536522a619ac9fe372c327e116b18f2cc6daa285
srilaxmipillalamarri <78467693+srilaxmipillalamarri@users.noreply.github.com>
2022-05-30 15:44:05 +0530

    day14_assignment

---
+++ srilaxmi pillalamarri/Foreachloop.java
@@ -0,0 +1,27 @@
+import java.util.*;  
+public class Foreachloop{  
+    public static void main(String[] args) {  
+          
+        List<String> list=new ArrayList<String>();  
+        list.add("ankit");  
+        list.add("mayank");  
+        list.add("irfan");  
+        list.add("jai");  
+          
+        list.forEach(  
+            (n)->System.out.println(n)  
+        );  
+    }  
+}  
+
+/*
+
+output:
+
+ankit
+mayank
+irfan
+jai
+
+
+*/
