55363e6ed3a629c5b5e99c0c2af669241e79dc47
anilghumare <105354744+anilghumare@users.noreply.github.com>
2022-05-30 17:17:34 +0530

    Assignment Day !4

---
+++ Anil Ghumare/InstanceMethodsReference2.java
@@ -0,0 +1,16 @@
+public class InstanceMethodReference2 {
+	public void printMsg() {
+		System.out.println("Hello ,this is Instance Method");
+
+	}
+
+	public static void main(String[] args) {
+		Thread t2 = new Thread(new InstanceMethodReference2()::printMsg);
+		t2.start();
+	}
+}
+
+/*
+Output:
+Hello ,this is Instance Method
+*/
---
+++ Anil Ghumare/JavaStreamExa2.java
@@ -0,0 +1,18 @@
+import java.util.stream.*;
+
+public class JavaStreamExample2 {
+	public static void main(String[] args) {
+		Stream.iterate(1, elements -> elements + 1).filter(elements -> elements % 5 == 0).limit(5)
+				.forEach(System.out::println);
+
+	}
+}
+
+/*
+Output:
+5
+10
+15
+20
+25
+*/
