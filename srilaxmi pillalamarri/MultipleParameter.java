78fd3bcc0429b27a53f7fae798486f2a94bcc85e
srilaxmipillalamarri <78467693+srilaxmipillalamarri@users.noreply.github.com>
2022-05-30 15:36:41 +0530

    day14_assignment

---
+++ srilaxmi pillalamarri/MultipleParameter.java
@@ -0,0 +1,29 @@
+//multiple paramters:
+interface Addable{
+int add(int a,int b);
+}
+
+public class MultipleParameter{
+public static void main(String[] args){
+
+//multiple paramters in lambda expression
+
+Addable ad1=(a,b)->(a+b);
+System.out.println(ad1.add(10,20));
+
+//multiple parameters with data type inlambda expression
+Addable ad2=(int a,int b)->(a+b);
+System.out.println(ad2.add(100,200));
+}
+}
+
+
+/*
+
+output:
+
+30
+300
+
+
+*/
