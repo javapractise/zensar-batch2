b1fde1b12f91714c03a1f3743de8bfeaddeb5f39
Jayshri261 <105355446+Jayshri261@users.noreply.github.com>
2022-05-31 15:52:09 +0530

    Add files via upload

---
+++ Jayshri Gaikwad/LocalDateExample.java
@@ -0,0 +1,14 @@
+import java.time.*;  
+public class LocalDateExample {  
+  public static void main(String[] args) {  
+    LocalDate date = LocalDate.of(2017, 1, 13);  
+    LocalDateTime datetime = date.atTime(1,50,9);      
+    System.out.println(datetime);   
+  }  
+}  
+
+
+/*
+Output:
+2017-01-13T01:50:09
+*/
