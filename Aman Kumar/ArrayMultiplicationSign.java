b5ee5e29578a4bf27d087aef814066d5611b5464
AK9773 <105356204+AK9773@users.noreply.github.com>
2022-06-01 15:57:08 +0530

    Test Done

---
+++ Aman Kumar/ArrayMultiplicationSign.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ArrayMultiplicationSign {
+	public static void main(String args[]) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Value of N: ");
+		int N = s.nextInt();
+		int[] Array = new int[N];
+		System.out.println("Enter Array Element: ");
+		for (int i = 0; i < N; i++) {
+			Array[i] = s.nextInt();
+		}
+		int Mul = 1;
+		for (int i = 0; i < N; i++) {
+			Mul = Mul * Array[i];
+		}
+		if (Mul == 0) {
+			System.out.println("0");
+		} else if (Mul < 0) {
+			System.out.println("-1");
+		} else {
+			System.out.println("1");
+		}
+
+	}
+}
